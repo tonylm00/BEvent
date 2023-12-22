0771e6715c14de1f9cb5ceb652cb233e88d80e3f
17Antony17 <antonioferraro1705@gmail.com>
2023-12-22 20:37:14 +0100

    Primo approccio a python e prima connessione con mongodb tramite login

--- ProvaAntonio.py
+++
@@ -1,7 +0,0 @@
-import site
-
-if __name__ == '__main__':
-    if __name__ == '__main__':
-        if __name__ == '__main__':
-            if site.main() is None:
-                site.main()
--- provaDaria.py
+++
@@ -1,1 +0,0 @@
-print("ciao sono daria simonetti")
