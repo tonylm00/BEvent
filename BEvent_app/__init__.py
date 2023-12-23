8766fcf35ef52bc22d0bbe7db7c0644dfd4082e6
17Antony17 <antonioferraro1705@gmail.com>
2023-12-23 16:22:54 +0100

    Creazione della struttura del progetto e sistemati reindirizzamenti del login

---
+++ BEvent_app/__init__.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+from pymongo import MongoClient
+import hashlib
+
+client = MongoClient('mongodb://localhost:27017/')
+db = client['BEvent']
+users_collection = db['Utente']
+
+
+def create_app():
+    app = Flask(__name__, static_folder='static')
+
+    @app.route('/')
+    def index():
+        return render_template('login.html')
+
+    @app.route('/login', methods=['POST'])
+    def login():
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = users_collection.find_one({'email': email, 'password': password})
+        if user:
+            return "Login riuscito!"
+        else:
+            return "Credenziali non valide. Riprova."
+
+    return app
--- app.py
+++ app.py
@@ -1,37 +1,6 @@
-from flask import Flask, render_template, request, redirect, url_for
-from pymongo import MongoClient
-import hashlib
+from BEvent_app import create_app
 
-app = Flask(__name__, static_folder='style')
-client = MongoClient('mongodb://localhost:27017/')
-db = client['BEvent']
-users_collection = db['Utente']
-
-
-def hash_password(password):
-    return hashlib.sha256(password.encode()).hexdigest()
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/login', methods=['POST'])
-def login():
-    email = request.form.get('email')
-    password = request.form.get('password')
-
-    #hashed_password = hash_password(password)  # Crittografia della password
-
-
-    #user = users_collection.find_one({'email': email, 'password': hashed_password})
-
-    user = users_collection.find_one({'email': email, 'password': password})
-    if user:
-        # Autenticazione riuscita
-        return "Login riuscito!"
-    else:
-
-        return "Credenziali non valide. Riprova."
+app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
