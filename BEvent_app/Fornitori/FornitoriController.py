afe03eae9fc28f16eee2c794455b6f6e747e35e7
hanstarzz <a.deluca104@studenti.unisa.it>
2023-12-29 11:36:38 +0100

    Fatta visualizzazione (va però implementato con la registrazione e la visualizzazione dei servizi del singolo fornitore), è stata scritta anche l'eliminazione ma da problemi sul reindirizzamento e sulla query.

---
+++ BEvent_app/Fornitori/FornitoriController.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template,Blueprint
+from .FornitoriService import get_tutti_servizi
+from BEvent_app import Routes
+
+
+
+
+
+Fornitori = Blueprint('Fornitori', __name__)
+
+
+@Fornitori.route('/fornitori')
+def Visualizza():  # put application's code here
+
+    servizi = get_tutti_servizi()
+    for servizio in servizi:
+        print("Documento Servizio:", servizio)
+    return render_template('AreaFornitore.html', servizi=servizi)
+
+from flask import redirect, url_for
+
+@Fornitori.route('/elimina_servizio/<servizio_id>')
+def elimina_servizio(servizio_id):
+    elimina_servizio(servizio_id)
+    return redirect(url_for('/fornitori'))
