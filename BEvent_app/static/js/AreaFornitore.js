dc5a4309dcb48ed50c06a4e605aa4c21c0454d19
carmineiaucci <iauccicarmine006@gmail.com>
2024-01-05 00:14:13 +0100

    area fornitore aggiornata in modo tale da contenere in futuro pubblicazione evento. migliorie CSS, HTML , javascript

---
+++ BEvent_app/static/js/AreaFornitore.js
@@ -0,0 +1,29 @@
+// Script per l'apertura e la chiusura della modale
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const modal = document.getElementById('myModalAF');
+    const button = document.getElementById("modal-buttonAF");
+
+document.getElementById('customButton').addEventListener('click', function() {
+    document.getElementById('fileInput').click();
+});
+    const openModal = function() {
+        modal.style.display = "block";
+        document.body.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+    };
+
+
+    const closeModal = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+            document.body.style.backgroundColor = "";
+        }
+    };
+
+
+    button.addEventListener('click', openModal, false);
+
+
+    window.addEventListener('click', closeModal, false);
+});
