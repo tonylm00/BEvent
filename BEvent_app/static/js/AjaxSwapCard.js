58f41dd24080547c6bcf0d645718bbb672e816b6
dariasimonetti <simonettidaria@gmail.com>
2024-01-06 22:06:06 +0100

    collegamento forntend e backend

--- BEvent_app/static/js/AjaxSwapCard.js
+++ BEvent_app/static/js/AjaxSwapCard.js
@@ -26,25 +26,32 @@ function aggiornaDOM(data) {
     if (data.fornitore_scelto) {
 
         let nuovoContenuto = `
-            <img style="box-shadow: #333333; margin-top: 50px;" class="img" src="data:image/jpeg;base64,${data.fornitore_scelto.foto[0]}" alt="Immagine">
-            <h1>${data.fornitore_scelto.nome_utente}</h1>
-            <p>${data.fornitore_scelto.descrizione}</p>
+            <div class="watch fade-in" style="display: flex; justify-items:center; justify-content: center;">
+                <img class="img" src="data:image/jpeg;base64,${data.fornitore_scelto.foto[0]}" alt="Immagine">
+            </div>
+            <h1 class="watch fade-in" style="text-align: center">${data.fornitore_scelto.nome_utente}</h1>
+            <p class="watch fade-in" style="text-align: center">${data.fornitore_scelto.descrizione}</p>
+            <p class="projTitle textGradient watch fade-in" style="font-size: 18px; margin-top: 10px;">Servizi offerti da ${data.fornitore_scelto.nome_utente}:</p>
+            <div class="grid-container-servizi">
+                <div class="grid-servizi">
         `;
 
         data.lista_servizi.forEach(servizio => {
             nuovoContenuto += `
-                <div class="container">
-                    <div class="card">
-                        <div class="face face1">
-                            <div class="content">
+                <div class="container2 watch fade-in">
+                    <div class="card2">
+                        <div class="face3 face4">
+                            <div class="content2">
                                 <img src="data:image/jpeg;base64,${servizio.foto_servizio[0]}">
                                 <h3>${servizio.tipo}</h3>
                             </div>
                         </div>
-                        <div class="face face2">
-                            <div class="content">
-                                <p><b>${servizio.tipo}<br></b><br>${servizio.descrizione}, ${servizio.prezzo}</p>
-                                <a href="#">Prenota</a>
+                        <div class="face3 face5">
+                            <div class="content2">
+                                <p>${servizio.descrizione}, ${servizio.prezzo}&euro;</p>
+                                <button style="border: none; background: transparent" id="${servizio._id}" onclick="salvaServizio('${servizio._id}')"> 
+                                    <a href="#">Prenota</a>
+                                </button>
                             </div>
                         </div>
                     </div>
@@ -52,9 +59,20 @@ function aggiornaDOM(data) {
             `;
         });
 
+        nuovoContenuto += `</div></div>`;
 
-        document.querySelector('.right-column').innerHTML = nuovoContenuto;
+
+        document.querySelector('.overflow-container').innerHTML = nuovoContenuto;
+
+        document.querySelectorAll('.overflow-container .watch').forEach(elemento => {
+            aggiungiElementoAllObserver(elemento);
+        });
     } else {
         console.error('Errore nella risposta:', data.errore);
     }
+}
+
+
+function aggiungiElementoAllObserver(element) {
+    observer.observe(element);
 }
