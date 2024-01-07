349218e125402b5a7db0b6f9b7e3d20de219338c
17Antony17 <antonioferraro1705@gmail.com>
2024-01-07 20:04:21 +0100

    Quasi completata la pagina del RiepilogoScelte.html

--- BEvent_app/static/js/Header3.js
+++ BEvent_app/static/js/Header3.js
@@ -1,9 +1,9 @@
 // Verifica durante la registrazione se è organizzatore o fornitore
     document.getElementById('dropdown').addEventListener('change', function () {
         var form = document.getElementById('registrazione');
-        if (this.value === 'organizzatore') {
+        if (this.value === '2') {
             form.action = "/registrazione_organizzatore_page";
-        } else if (this.value === 'fornitore') {
+        } else if (this.value === '3') {
             form.action = "/registrazione_page";
         }
     });
