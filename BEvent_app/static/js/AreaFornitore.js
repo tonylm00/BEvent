47f03069b4c0c768e69be666f00f0ecffad3338d
linux <l.damora3@studenti.unisa.it>
2024-01-08 12:28:37 +0100

    modifiche al front end di area fornitore

--- BEvent_app/static/js/AreaFornitore.js
+++ BEvent_app/static/js/AreaFornitore.js
@@ -1,6 +1,40 @@
 // Script per l'apertura e la chiusura della modale
 
+document.addEventListener('DOMContentLoaded', function() {
+    var form = document.getElementById('uploadForm'); // Assicurati che questo sia l'ID del tuo form
 
+    form.onsubmit = function(event) {
+        event.preventDefault(); // Previene il comportamento di default del form
+
+        var formData = new FormData(form);
+        fetch('/aggiungi_foto_fornitore', { // URL a cui inviare la richiesta POST
+            method: 'POST',
+            body: formData
+        })
+        .then(response => response.json()) // Presumendo che il server risponda con JSON
+        .then(data => {
+            if(data.success) {
+                // Aggiungi le immagini caricate alla tabella
+                var imageCell = document.getElementById('imageCell'); // Assicurati che ci sia un TD con questo ID
+                data.images.forEach(image => {
+                    var img = document.createElement('img');
+                    img.src = image.path; // O usa 'data:image/jpeg;base64,' + image.base64 se è codificata in base64
+                    img.alt = 'Foto del Servizio';
+                    img.height = 100; // Imposta l'altezza desiderata
+                    imageCell.appendChild(img);
+                });
+            } else {
+                // Gestisci l'errore
+                console.error('Caricamento non riuscito');
+            }
+        })
+        .catch(error => {
+            // Gestisci eventuali errori di rete
+            console.error('Errore di rete:', error);
+        });
+    };
+});
+// Script per l'apertura e la chiusura della modale
 document.addEventListener('DOMContentLoaded', function() {
     const modal = document.getElementById('myModalAF');
     const button = document.getElementById("modal-buttonAF");
@@ -24,4 +58,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
     window.addEventListener('click', closeModal, false);
+
+
 });
