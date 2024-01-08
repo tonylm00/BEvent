64d5b1362d4253facac53ea5f8f1e3d4bd83a3b4
linux <l.damora3@studenti.unisa.it>
2024-01-08 12:36:42 +0100

    modifiche

--- BEvent_app/static/js/AreaFornitore.js
+++ BEvent_app/static/js/AreaFornitore.js
@@ -1,35 +1,35 @@
 // Script per l'apertura e la chiusura della modale
 
 document.addEventListener('DOMContentLoaded', function() {
-    var form = document.getElementById('uploadForm'); // Assicurati che questo sia l'ID del tuo form
+    var form = document.getElementById('uploadForm');
 
     form.onsubmit = function(event) {
-        event.preventDefault(); // Previene il comportamento di default del form
+        event.preventDefault(); /
 
         var formData = new FormData(form);
-        fetch('/aggiungi_foto_fornitore', { // URL a cui inviare la richiesta POST
+        fetch('/aggiungi_foto_fornitore', { /
             method: 'POST',
             body: formData
         })
-        .then(response => response.json()) // Presumendo che il server risponda con JSON
+        .then(response => response.json())
         .then(data => {
             if(data.success) {
                 // Aggiungi le immagini caricate alla tabella
-                var imageCell = document.getElementById('imageCell'); // Assicurati che ci sia un TD con questo ID
+                var imageCell = document.getElementById('imageCell');
                 data.images.forEach(image => {
                     var img = document.createElement('img');
-                    img.src = image.path; // O usa 'data:image/jpeg;base64,' + image.base64 se è codificata in base64
+                    img.src = image.path;
                     img.alt = 'Foto del Servizio';
-                    img.height = 100; // Imposta l'altezza desiderata
+                    img.height = 100;
                     imageCell.appendChild(img);
                 });
             } else {
-                // Gestisci l'errore
+
                 console.error('Caricamento non riuscito');
             }
         })
         .catch(error => {
-            // Gestisci eventuali errori di rete
+
             console.error('Errore di rete:', error);
         });
     };
