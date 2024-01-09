cba426c51cbbadd8fad627a65757cc3d48936f41
17Antony17 <antonioferraro1705@gmail.com>
2024-01-09 13:11:46 +0100

    Pagina TrovaEventi front e back end + piccole modifiche RiepilogoScelte.js

--- BEvent_app/static/js/RiepilogoScelte.js
+++ BEvent_app/static/js/RiepilogoScelte.js
@@ -4,10 +4,13 @@
         // Imposta l'attributo "action" del form in base all'azione selezionata
         if (azione === 'Salva_bozza') {
             formDati.action = '/salva_evento_come_bozza';
+            formDati.method= "post"
         } else if (azione === 'Prenota_evento') {
             formDati.action = '/Prenota';
+            formDati.method= "post"
          } else if (azione === 'Elimina') {
             formDati.action = '/annulla_creazione_evento'
+            formDati.method= "post"
             return;
         }
 
