0470d9063854a73cd983d9f38e5e332e71154590
hanst <a.deluca104@studenti.unisa.it>
2024-01-19 17:31:20 +0100

    Invio recensione fatto e finito

--- BEvent_app/Fornitori/FornitoriController.py
+++ BEvent_app/Fornitori/FornitoriController.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template, Blueprint, request, session
+from flask import Flask, render_template, Blueprint, request, session, flash
 from flask_login import current_user
 from ..InterfacciaPersistenza.ServizioOfferto import Servizio_Offerto
-from .FornitoriService import get_dati_fornitore, get_tutti_servizi_byFornitore,aggiorna_foto_fornitore,aggiungi_servizio,modifica_servizio,elimina_servizio,get_eventi_ByFornitorePrivato,getEventi_FornitorePubblico,cancella_evento,get_dettagli_evento,get_dati_organizzatore,get_dati_servizi,invio_feedBack
+from .FornitoriService import get_dati_fornitore, get_tutti_servizi_byFornitore, aggiorna_foto_fornitore, \
+    aggiungi_servizio, modifica_servizio, elimina_servizio, get_eventi_ByFornitorePrivato, getEventi_FornitorePubblico, \
+    cancella_evento, get_dettagli_evento, get_dati_organizzatore, get_dati_servizi, invio_feedBack, \
+    validate_servizio_data
 from BEvent_app import Routes
 from flask import redirect, url_for
 from ..Utils import Image
@@ -9,43 +12,7 @@ from ..Routes import home, fornitore_page,visualizza_evento_dettagli_page
 
 
 Fornitori = Blueprint('Fornitori', __name__)
-def is_valid_number(value):
-    try:
-        float(value)
-        return True
-    except ValueError:
-        return False
 
-def validate_servizio_data(descrizione, tipo, prezzo, quantita):
-    '''
-    serve un validare i dati di servizio
-
-    :param descrizione: str
-    :param tipo: str
-    :param prezzo: float
-    :param quantita: int
-
-    :return: messaggi di errore nel caso in cui uno dei campi non è valido ( con annessa descrizione del problema ),
-    True se invece i campi sono validi
-
-    '''
-    # Verifica se la descrizione supera i 500 caratteri
-    if len(descrizione) > 500:
-        return False, "La descrizione non deve superare i 500 caratteri."
-
-    # Verifica se il tipo supera i 25 caratteri
-    if len(tipo) > 25:
-        return False, "Il tipo non deve superare i 25 caratteri."
-
-    # Verifica se il prezzo è un numero positivo
-    if not is_valid_number(prezzo) or float(prezzo) < 0:
-        return False, "Il prezzo deve essere un numero non negativo."
-
-    # Verifica se la quantità è un numero intero positivo
-    if not quantita.isdigit() or int(quantita) < 0:
-        return False, "La quantità deve essere un numero intero non negativo."
-
-    return True, None
 
 
 @Fornitori.route('/fornitori', methods=['GET', 'POST'])
@@ -67,8 +34,8 @@ def visualizza_controller():
 
 def aggiungi_foto_fornitore_controller():
     '''
-
-    :return:
+    Aggiunge una foto al fornitore
+    :return: reindirizza alla a pagina del fornitore
     '''
     files = request.files.getlist('foto')
     id_fornitore = session['id']
@@ -93,8 +60,8 @@ def aggiungi_foto_fornitore_controller():
 @Fornitori.route('/elimina_servizio_areaFornitore/', methods=['POST'])
 def elimina_servizio_controller():
     '''
-
-    :return:
+    Elimina servizio scelto dal fornitore
+    :return: reindirizza alla a pagina del fornitore
     '''
     servizio_id = request.form.get('servizio_id')
     elimina_servizio(servizio_id)
@@ -104,15 +71,13 @@ def elimina_servizio_controller():
 @Fornitori.route('/modifica_servizio/', methods=['POST'])
 def modifica_servizio_controller():
     '''
-
-    :return:
+    Modifica servizio scelto dal fornitore
+    :return: reindirizza alla a pagina del fornitore
     '''
     descrizione = request.form.get("descrizione")
     tipo = request.form.get("tipo")
     prezzo = request.form.get("prezzo")
     quantita = request.form.get("quantità")
-    is_valid, error_message = validate_servizio_data(descrizione, tipo, prezzo, quantita)
-    if not is_valid: return error_message
     nuovi_dati = {
         "Descrizione": descrizione,
         "Tipo": tipo,
@@ -149,8 +114,6 @@ def aggiungi_servizio_controller():
     tipo = request.form.get("tipo")
     prezzo = request.form.get("prezzo")
     quantita = request.form.get("quantità")
-    is_valid, error_message = validate_servizio_data(descrizione, tipo, prezzo, quantita)
-    if not is_valid: return error_message
     nuovi_dati = {
         "Descrizione": descrizione,
         "Tipo":  tipo,
@@ -161,14 +124,16 @@ def aggiungi_servizio_controller():
         "isDeleted": False,
         "isCurrentVersion" : None
     }
-    aggiungi_servizio(nuovi_dati)
-    return redirect('/fornitori')
-
+    result = aggiungi_servizio(nuovi_dati)
+    if result==True:
+        return redirect('/fornitori')
+    else:
+        return "errore"
 @Fornitori.route('/elimina_evento_pubblico', methods=['POST'])
 def elimina_evento_controller():
     '''
-
-    :return:
+    elimina un evento pubblico
+    :return: reindirizza alla a pagina del fornitore
     '''
     id = request.form.get("id")
     cancella_evento(id)
@@ -177,6 +142,11 @@ def elimina_evento_controller():
 
 @Fornitori.route('/Visuallizza_Dettagli_evento_Fornitore',  methods=['GET', 'POST'])
 def visualizza_evento_dettagli_controller():
+    '''
+    Visualizza i dettagli dell'evento in cui è stato prenotato il servizio del fornitore
+    :return: reindirizza alla pagina dettagli dell'evento
+
+    '''
     id = request.form.get("id")
     evento = get_dettagli_evento(id)
     organizzatore = get_dati_organizzatore(id)
@@ -184,6 +154,11 @@ def visualizza_evento_dettagli_controller():
     return visualizza_evento_dettagli_page(evento = evento, organizzatore =organizzatore, servizi=servizi)
 @Fornitori.route('/invio_Feedback', methods=['POST'])
 def invio_FeedBack_controller():
+    '''
+    funzione che si 
+    :return:
+
+    '''
     id = request.form.get("valutato")
     valutazione = request.form.get("valutazione")
     invio_feedBack(id, session["id"], valutazione)
--- BEvent_app/Fornitori/FornitoriService.py
+++ BEvent_app/Fornitori/FornitoriService.py
@@ -1,4 +1,5 @@
 from bson import ObjectId
+from flask import flash
 from pymongo import MongoClient
 
 
@@ -7,7 +8,43 @@ from ..InterfacciaPersistenza import ServizioOfferto
 from ..InterfacciaPersistenza import Organizzatore
 
 
+def is_valid_number(value):
+    try:
+        float(value)
+        return True
+    except ValueError:
+        return False
 
+def validate_servizio_data(descrizione, tipo, prezzo, quantita):
+    '''
+    serve un validare i dati di servizio
+
+    :param descrizione: str
+    :param tipo: str
+    :param prezzo: float
+    :param quantita: int
+
+    :return: messaggi di errore nel caso in cui uno dei campi non è valido ( con annessa descrizione del problema ),
+    True se invece i campi sono validi
+
+    '''
+    # Verifica se la descrizione supera i 500 caratteri
+    if len(descrizione) > 500:
+        flash("La descrizione non deve superare i 500 caratteri", "error")
+        return False
+
+
+    # Verifica se il tipo supera i 25 caratteri
+    if tipo not in ['Location','Fiorai e Decorazione','Catering','Pasticceria','Musica e Servizio Audio','Intrattenimento','Animazione per bambini','Fotografo','Servizi di Trasporto','Gadget','Altro']:
+        flash("Il tipo deve essere uno di quelli selezionati", "error")
+        return False
+
+    # Verifica se il prezzo è un numero positivo
+    if not is_valid_number(prezzo) or float(prezzo) < 0:
+        flash("Il prezzo deve essere un numero non negativo", "error")
+        return False
+    flash("Aggiunta avvenuto con successo","succes")
+    return True
 
 def get_tutti_servizi_byFornitore(id_fornitore):
     db = get_db()
@@ -134,8 +171,14 @@ def modifica_servizio(nuovi_dati, servizio_id):
 
 
 def aggiungi_servizio(nuovi_dati):
-    db = get_db()
-    db['Servizio Offerto'].insert_one(nuovi_dati)
+    result = validate_servizio_data(nuovi_dati['Descrizione'],nuovi_dati['Tipo'], nuovi_dati['Prezzo'],2)
+    if result==True:
+        db = get_db()
+        db['Servizio Offerto'].insert_one(nuovi_dati)
+        return True
+    else: return False
+
+
 
 
 def get_eventi_ByFornitorePrivato(id):
@@ -237,3 +280,5 @@ def get_fornitori(id_fornitori):
         lista_fornitori.append(Fornitore(user_data, user_data))
 
     return lista_fornitori
+
+
