28be1cfa589ec23fc28c6f4530ff08a3ac644732
17Antony17 <antonioferraro1705@gmail.com>
2024-01-19 20:18:30 +0100

    Aggiustata la scrittura recensione per i servizi offerti

--- BEvent_app/FeedBack/FeedBackController.py
+++ BEvent_app/FeedBack/FeedBackController.py
@@ -1,4 +1,4 @@
-from flask import request, Blueprint, session, redirect
+from flask import request, Blueprint, session, redirect, url_for
 from .FeedBackService import  inserisci_recensione
 fb = Blueprint('fb', __name__)
 
@@ -6,11 +6,10 @@ fb = Blueprint('fb', __name__)
 @fb.route('/invio_recensione', methods=['POST'])
 def invio_recensione():
 
-    print("invio_recensione")
-    id_valutato = request.form.get('id')
-    id_valutante = session['id']
-    voto = request.form.get('voto')
-    titolo = request.form.get('titolo')
-    descrizione = request.form.get('descrizione')
-    inserisci_recensione(id_valutato,id_valutante,voto,titolo,descrizione)
-    return "fatto"
+ id_valutato = request.form.get('id')
+ id_valutante = session['id']
+ voto = request.form.get('voto')
+ titolo = request.form.get('titolo')
+ descrizione = request.form.get('descrizione')
+ inserisci_recensione(id_valutato,id_valutante,voto,titolo,descrizione)
+ return redirect(url_for('aut.area_organizzatore'))
--- BEvent_app/static/js/ScritturaRecensioni.js
+++ BEvent_app/static/js/ScritturaRecensioni.js
@@ -1,10 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Create stars for each rating type
-    createStarRating('overall-rating');
-    createStarRating('scenografia-rating');
-    createStarRating('accoglienza-rating');
-    createStarRating('professionalita-rating');
-
 
    function createStarRating(ratingId) {
     const container = document.getElementById(ratingId);
@@ -30,24 +24,4 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 
-    document.getElementById('submit-review').addEventListener('click', function() {
-        // Logic to handle the submission of the review
-        const title = document.getElementById('review-title').value;
-        const body = document.getElementById('review-body').value;
-        const overallRating = getRatingValue('overall-rating');
-        const hydrationRating = getRatingValue('scenografia-rating');
-        const freshnessRating = getRatingValue('accoglienza-rating');
-        const deepcleanRating = getRatingValue('professionalita-rating');
-
-        console.log({
-            title,
-            body,
-            ratings: {
-                overall: overallRating,
-                scenografia: scenografiaRating,
-                accoglienza: accoglienzaRating,
-                professionalita: professionalitaRating
-            }
-        });
-    });
 });
