222297062b9d1704ea1bf1ed086dcc878aeb1555
linux <l.damora3@studenti.unisa.it>
2024-01-20 03:04:24 +0100

    pydoc interfacciapersistenza

--- BEvent_app/InterfacciaPersistenza/Biglietto.py
+++ BEvent_app/InterfacciaPersistenza/Biglietto.py
@@ -1,5 +1,26 @@
 class Biglietto:
+    """
+        Classe che rappresenta un biglietto per un evento.
+
+        Args:
+            biglietto_data (dict): Dati del biglietto.
+
+        Attributi:
+            id (str): Identificatore univoco del biglietto.
+            evento_associato (str): ID dell'evento associato al biglietto.
+            compratore (str): Nome del compratore del biglietto.
+            data_evento (str): Data dell'evento associato al biglietto.
+            nome_evento (str): Nome dell'evento associato al biglietto.
+            dove (str): Luogo dell'evento associato al biglietto.
+            ora (str): Ora dell'evento associato al biglietto.
+        """
     def __init__(self, biglietto_data):
+        """
+               Inizializza un nuovo oggetto Biglietto.
+
+               Args:
+                   biglietto_data (dict): Dati del biglietto.
+               """
         self.id = str(biglietto_data['_id'])
         self.evento_associato = biglietto_data['Evento_associato']
         self.compratore = biglietto_data['CompratoDa']
